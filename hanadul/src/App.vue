284e35d12cc5d48e1a968d8d0eea823c7ed119f4
LudaStar <wlgusdlaek@naver.com>
2022-04-17 19:50:48 +0900

    Fix : Mypage View

--- hanadul/src/App.vue
+++ hanadul/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/signin">Sign In</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/signup">Sign Up</router-link>
   </nav>
   <router-view />
 </template>
