7d506861a2431b94a9cd9a5adae7c4cdecbb08b2
LudaStar <wlgusdlaek@naver.com>
2022-04-23 22:41:37 +0900

    Feat. Mypage view

---
+++ hanadul/src/views/profile/MyPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mypage">
+    <user-info></user-info>
+    <div style="height: 40px"></div>
+    <v-tabs>
+      <v-tab>운영 PJT</v-tab>
+      <v-tab>진행 PJT</v-tab>
+      <v-tab>대기중인 PJT</v-tab>
+    </v-tabs>
+    <user-pjt></user-pjt>
+    <div style="height: 30px"></div>
+    <user-pjt></user-pjt>
+  </div>
+</template>
+
+<script>
+import UserInfo from '../../components/profile/UserInfo.vue'
+import UserPjt from '../../components/profile/UserPjt.vue'
+
+export default {
+  components: { UserInfo, UserPjt },
+}
+</script>
