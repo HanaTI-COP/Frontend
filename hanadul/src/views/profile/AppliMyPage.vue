2729e9edbb3cfd68d07894872dfcec82010369dd
JungHwan_yoon <34881277+dhfkdlxm@users.noreply.github.com>
2022-05-01 16:29:31 +0900

    지원자페이지수정

---
+++ hanadul/src/views/profile/AppliMyPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="appliuserpage">
+    <appli-info></appli-info>
+    <div style="height: 40px"></div>
+    <v-tabs>
+      <v-tab>PJT 이력</v-tab>
+    </v-tabs>
+    <appli-pjt></appli-pjt>
+    <div style="height: 30px"></div>
+    <appli-pjt></appli-pjt>
+  </div>
+</template>
+
+<script>
+import AppliInfo from '../../components/profile/AppliUserInfo.vue'
+import AppliPjt from '../../components/profile/AppliUserPjt.vue'
+
+export default {
+  components: { AppliInfo, AppliPjt },
+}
+</script>
+<style>
+.appliuserpage{
+  width:1200px;
+  margin:auto
+}
+</style>
